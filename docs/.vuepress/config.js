35cce51295af526e49f3ac57cea337cc922f01e1
youcome <2448485734@qq.com>
2023-04-10 21:36:38 +0800

    frist blog commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    title: '网站的标题',  
+    description: '网站描述', // 它将会以 <meta> 标签渲染到当前页面的 HTML 中。
+    head: [
+      ['link', { rel: 'icon', href: '/logo.jpg' }] // 需要被注入到当前页面的 HTML <head> 中的标签
+    ],
+}
